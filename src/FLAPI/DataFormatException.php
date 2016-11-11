42ace2a12cb89e8a05de930544aa1004b0b36b0b
Alexander Jank <himself@alexanderjank.de>
2016-11-11 06:26:23 +0100

    Error Output: added new Exception classes

---
+++ src/FLAPI/DataFormatException.php
@@ -0,0 +1,12 @@
+<?php
+namespace FLAPI;
+
+/**
+ * @class DataFormatException
+ * @author Alexander Jank <himself@alexanderjank.de>
+ * @license GNU GPL v3.0
+ * @package FLAPI
+ */
+class DataFormatException extends \Exception {
+
+};
---
+++ src/FLAPI/SenderNotFoundException.php
@@ -0,0 +1,12 @@
+<?php
+namespace FLAPI;
+
+/**
+ * @class SenderNotFoundException
+ * @author Alexander Jank <himself@alexanderjank.de>
+ * @license GNU GPL v3.0
+ * @package FLAPI
+ */
+class SenderNotFoundException extends \Exception {
+
+};
