edc040e5e7596459c1b2ebcab3a511ba95f2232e
Alexander Jank <himself@alexanderjank.de>
2016-11-11 06:49:32 +0100

    Denglisch: renamed error classes

--- src/FLAPI/ChannelNotFoundException.php
+++ src/FLAPI/ChannelNotFoundException.php
@@ -2,11 +2,11 @@
 namespace FLAPI;
 
 /**
- * @class SenderNotFoundException
+ * @class ChannelNotFoundException
  * @author Alexander Jank <himself@alexanderjank.de>
  * @license GNU GPL v3.0
  * @package FLAPI
  */
-class SenderNotFoundException extends \Exception {
+class ChannelNotFoundException extends \Exception {
 
 };
