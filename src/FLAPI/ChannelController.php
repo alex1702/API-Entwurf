925beb488ec27496d65728bbbfcadd16581956cc
Christoph "criztovyl" Schulz <ch.schulz@joinout.de>
2016-11-15 01:00:10 +0100

    Soft-tabs

--- src/FLAPI/ChannelController.php
+++ src/FLAPI/ChannelController.php
@@ -11,182 +11,182 @@ use Psr\Http\Message\ServerRequestInterface;
  */
 class ChannelController {
 
-	/**
-	 * @var \Interop\Container\ContainerInterface
-	 */
-	protected $ci;
+    /**
+     * @var \Interop\Container\ContainerInterface
+     */
+    protected $ci;
 
-	/**
-	 * @var \Illuminate\Database\Capsule\Manager
-	 */
-	protected $db;
+    /**
+     * @var \Illuminate\Database\Capsule\Manager
+     */
+    protected $db;
 
-	/**
-	 * @var \Illuminate\Database\Query\Builder
-	 */
-	protected $senderTable;
+    /**
+     * @var \Illuminate\Database\Query\Builder
+     */
+    protected $senderTable;
 
-	/**
-	 * @var \Illuminate\Database\Query\Builder
-	 */
-	protected $sendungenTable;
+    /**
+     * @var \Illuminate\Database\Query\Builder
+     */
+    protected $sendungenTable;
 
-	/**
-	 * @var \Illuminate\Database\Query\Builder
-	 */
-	protected $downloadTable;
+    /**
+     * @var \Illuminate\Database\Query\Builder
+     */
+    protected $downloadTable;
 
-	/**
-	 * @param \Interop\Container\ContainerInterface $ci
-	 * @param \Illuminate\Database\Capsule\Manager $db
-	 */
-	public function __construct(\Interop\Container\ContainerInterface $ci, \Illuminate\Database\Capsule\Manager $db) {
-		$this->ci = $ci;
-		$this->senderTable = $db->table('sender');
-		$this->sendungenTable = $db->table('sendungen');
-		$this->downloadTable = $db->table('download');
-		$this->db = $db;
-	}
+    /**
+     * @param \Interop\Container\ContainerInterface $ci
+     * @param \Illuminate\Database\Capsule\Manager $db
+     */
+    public function __construct(\Interop\Container\ContainerInterface $ci, \Illuminate\Database\Capsule\Manager $db) {
+        $this->ci = $ci;
+        $this->senderTable = $db->table('sender');
+        $this->sendungenTable = $db->table('sendungen');
+        $this->downloadTable = $db->table('download');
+        $this->db = $db;
+    }
 
-	/**
-	 * Route-method for /sender
-	 *
-	 * @param \Psr\Http\Message\ServerRequestInterface $request
-	 * @param \Psr\Http\Message\ResponseInterface $response
-	 * @param array $args
-	 * @return \Psr\Http\Message\ResponseInterface
-	 */
-	public function getChannelList(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
-		$senderData = $this->senderTable->get(['name', 'abbr'])->toArray();
-		foreach($senderData as &$sender) {
-			$sender->url = $this->ci->get('router')->pathFor('senderFull', [
-				'abbr' => $sender->abbr
-			]);
-		}
+    /**
+     * Route-method for /sender
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getChannelList(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $senderData = $this->senderTable->get(['name', 'abbr'])->toArray();
+        foreach($senderData as &$sender) {
+            $sender->url = $this->ci->get('router')->pathFor('senderFull', [
+                'abbr' => $sender->abbr
+            ]);
+        }
 
-		return $this->ci->dataFormatter->format($response, $senderData, $request->getAttribute('format'));
-	}
+        return $this->ci->dataFormatter->format($response, $senderData, $request->getAttribute('format'));
+    }
 
-	/**
-	 * Route-method for /sender/{abbr}
-	 *
-	 * @param \Psr\Http\Message\ServerRequestInterface $request
-	 * @param \Psr\Http\Message\ResponseInterface $response
-	 * @param array $args
-	 * @return \Psr\Http\Message\ResponseInterface
-	 */
-	public function getChannelAllShows(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
-		$senderId = $this->senderTable->where('abbr', '=', $args['abbr'])->get(['id'])->toArray();
-		if(!isset($senderId[0]->id)) {
-			throw new ChannelNotFoundException('Sender not found!');
-		}
-		$sendungen = $this->sendungenTable->where('sender', '=', $senderId[0]->id)->get(['title', 'date', 'length', 'id'])->toArray();
-		foreach($sendungen as &$sendung) {
-			$sendung->date = strtotime($sendung->date);
-			$sendung->length = $this->timeToSec($sendung->length);
+    /**
+     * Route-method for /sender/{abbr}
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getChannelAllShows(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $senderId = $this->senderTable->where('abbr', '=', $args['abbr'])->get(['id'])->toArray();
+        if(!isset($senderId[0]->id)) {
+            throw new ChannelNotFoundException('Sender not found!');
+        }
+        $sendungen = $this->sendungenTable->where('sender', '=', $senderId[0]->id)->get(['title', 'date', 'length', 'id'])->toArray();
+        foreach($sendungen as &$sendung) {
+            $sendung->date = strtotime($sendung->date);
+            $sendung->length = $this->timeToSec($sendung->length);
 
-			$sendung = downloadsArray($sendung, $this->downloadTable->where('sendung', '=', $sendung->id)->get(['url', 'quality']));
+            $sendung = downloadsArray($sendung, $this->downloadTable->where('sendung', '=', $sendung->id)->get(['url', 'quality']));
 
-			unset($sendung->id);
-		}
+            unset($sendung->id);
+        }
 
-		return $this->ci->dataFormatter->format($response, $sendungen, $request->getAttribute('format'));
-	}
+        return $this->ci->dataFormatter->format($response, $sendungen, $request->getAttribute('format'));
+    }
 
-	/**
-	 * Route-method for /sender/{abbr}/{timeframe}
-	 *
-	 * @param \Psr\Http\Message\ServerRequestInterface $request
-	 * @param \Psr\Http\Message\ResponseInterface $response
-	 * @param array $args
-	 * @return \Psr\Http\Message\ResponseInterface
-	 */
-	public function getChannelNarrowdShows(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
-		$senderId = $this->senderTable->where('abbr', '=', $args['abbr'])->get(['id'])->toArray();
-		if(!isset($senderId[0]->id)) {
-			throw new ChannelNotFoundException('Sender not found!');
-		}
-		list($start, $end) = $this->parseTimeframe($args['timeframe']);
-		$sqlStart = date("Y-m-d H:i:s", $start);
-		$sqlEnd = date("Y-m-d H:i:s", $end);
-		$sendungen = $this->sendungenTable->where('sender', '=', $senderId[0]->id)->whereDate('date', '>=', $sqlStart)->whereDate('date', '<=', $sqlEnd)->get(['title', 'date', 'length', 'id'])->toArray();
-		foreach($sendungen as &$sendung) {
-			$sendung->date = strtotime($sendung->date);
-			$sendung->length = $this->timeToSec($sendung->length);
+    /**
+     * Route-method for /sender/{abbr}/{timeframe}
+     *
+     * @param \Psr\Http\Message\ServerRequestInterface $request
+     * @param \Psr\Http\Message\ResponseInterface $response
+     * @param array $args
+     * @return \Psr\Http\Message\ResponseInterface
+     */
+    public function getChannelNarrowdShows(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $senderId = $this->senderTable->where('abbr', '=', $args['abbr'])->get(['id'])->toArray();
+        if(!isset($senderId[0]->id)) {
+            throw new ChannelNotFoundException('Sender not found!');
+        }
+        list($start, $end) = $this->parseTimeframe($args['timeframe']);
+        $sqlStart = date("Y-m-d H:i:s", $start);
+        $sqlEnd = date("Y-m-d H:i:s", $end);
+        $sendungen = $this->sendungenTable->where('sender', '=', $senderId[0]->id)->whereDate('date', '>=', $sqlStart)->whereDate('date', '<=', $sqlEnd)->get(['title', 'date', 'length', 'id'])->toArray();
+        foreach($sendungen as &$sendung) {
+            $sendung->date = strtotime($sendung->date);
+            $sendung->length = $this->timeToSec($sendung->length);
 
-			$sendung = downloadsArray($sendung, $this->downloadTable->where('sendung', '=', $sendung->id)->get(['url', 'quality']));
+            $sendung = downloadsArray($sendung, $this->downloadTable->where('sendung', '=', $sendung->id)->get(['url', 'quality']));
 
-			unset($sendung->id);
-		}
+            unset($sendung->id);
+        }
 
-		return $this->ci->dataFormatter->format($response, $sendungen, $request->getAttribute('format'));
-	}
+        return $this->ci->dataFormatter->format($response, $sendungen, $request->getAttribute('format'));
+    }
 
-	/**
-	 * Method for converting MySQL time to seconds
-	 *
-	 * @param string $time
-	 * @return int
-	 */
-	private function timeToSec(string $time): int {
-		$sec = 0;
-		foreach (array_reverse(explode(':', $time)) as $k => $v) {
-			$sec += pow(60, $k) * $v;
-		}
-		return $sec;
-	}
+    /**
+     * Method for converting MySQL time to seconds
+     *
+     * @param string $time
+     * @return int
+     */
+    private function timeToSec(string $time): int {
+        $sec = 0;
+        foreach (array_reverse(explode(':', $time)) as $k => $v) {
+            $sec += pow(60, $k) * $v;
+        }
+        return $sec;
+    }
 
-	/**
-	 * Mathod for parsing the user-given timeframe from the URL to a start and end
-	 *
-	 * @param string $timeframe
-	 * @return array
-	 */
-	private function parseTimeframe(string $timeframe): array {
-		$regex = '/(\d+)([td])(\d+)/';
-		preg_match($regex, $timeframe, $matches);
-		$method = $matches[2];
-		$start = $matches[1];
-		$end = $matches[3];
-		switch($method) {
-			case "t":
-			break;
+    /**
+     * Mathod for parsing the user-given timeframe from the URL to a start and end
+     *
+     * @param string $timeframe
+     * @return array
+     */
+    private function parseTimeframe(string $timeframe): array {
+        $regex = '/(\d+)([td])(\d+)/';
+        preg_match($regex, $timeframe, $matches);
+        $method = $matches[2];
+        $start = $matches[1];
+        $end = $matches[3];
+        switch($method) {
+            case "t":
+            break;
 
-			case "d":
-			$start = time() - ($start * 86400);
-			$end = ($end * 86400) + time();
-			break;
+            case "d":
+            $start = time() - ($start * 86400);
+            $end = ($end * 86400) + time();
+            break;
 
-			default:
-			throw new \Exception('Timeframe-type not supported!');
-			break;
-		}
-		return [(int) $start, (int) $end];
-	}
+            default:
+            throw new \Exception('Timeframe-type not supported!');
+            break;
+        }
+        return [(int) $start, (int) $end];
+    }
 
-	/**
-	 * Method for generating downloads array for a show
-	 *
-	 * @param sendung the show to add the downloads to
-	 * @param downloads the downloads to add
-	 *
-	 * @return sendung the new sendung with the downloads added
-	 */
-	private function downloadsArray($sendung, $downloads){
+    /**
+     * Method for generating downloads array for a show
+     *
+     * @param sendung the show to add the downloads to
+     * @param downloads the downloads to add
+     *
+     * @return sendung the new sendung with the downloads added
+     */
+    private function downloadsArray($sendung, $downloads){
 
-			$sendung->download = array();
+            $sendung->download = array();
 
-			foreach($downloads as $download){
+            foreach($downloads as $download){
 
-				if(!isset($sendung->download[$download->quality])){
-					// Create array with element at key
-					$sendung->download[$download->quality] = array($download->url);
+                if(!isset($sendung->download[$download->quality])){
+                    // Create array with element at key
+                    $sendung->download[$download->quality] = array($download->url);
                 }
-				else {
-					// Push element to array at key
-					$sendung->download[$download->quality][] = $download->url;
+                else {
+                    // Push element to array at key
+                    $sendung->download[$download->quality][] = $download->url;
                 }
 
             }
-	}
+    }
 }
